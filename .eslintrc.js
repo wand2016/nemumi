82821f0987f71b964ce1b94544664e85ae3a3e4d
wand2016 <d19921207@gmail.com>
2022-04-20 02:15:44 +0900

    eslintルール修正: 不使用変数・仮引数のignoreパターン設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,5 +38,12 @@ module.exports = {
         },
       },
     ],
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        varsIgnorePattern: "^_",
+        argsIgnorePattern: "^_",
+      },
+    ],
   },
 };
