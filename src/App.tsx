34cab14621fa637bbee4755fe3641a2ebc2e1df5
wand2016 <d19921207@gmail.com>
2022-04-19 21:56:39 +0900

    wip checkstate

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import useSWR from "swr";
 import axios from "axios";
 import { baseUrl, endpoints } from "./api";
 import { PrefectureResponse } from "./api/Prefecture";
+import { useEffect, useState } from "react";
 
 const fetcher = async (url: string) => {
   const axiosInstance = axios.create({
@@ -16,23 +17,62 @@ const fetcher = async (url: string) => {
 };
 
 function App() {
+  type PrefectureCheckState = Record<string, boolean>;
+  const [prefectureCheckState, setPrefectureCheckState] =
+    useState<PrefectureCheckState>({});
+
   const { data: prefectures, error } = useSWR<PrefectureResponse>(
     endpoints.prefectures,
     fetcher
   );
 
+  useEffect(() => {
+    if (!prefectures) return;
+
+    const initState: PrefectureCheckState = prefectures.result.reduce(
+      (prev, { prefCode }) => ({
+        ...prev,
+        [prefCode]: false,
+      }),
+      {}
+    );
+
+    setPrefectureCheckState(initState);
+  }, [prefectures]);
+
   if (error) return <div>failed to load</div>;
   if (!prefectures) return <div>loading...</div>;
 
+  const checked = (prefCode: number) => prefectureCheckState[`${prefCode}`];
+  const toggle = (prefCode: number) => {
+    setPrefectureCheckState((prevState) => ({
+      ...prevState,
+      [prefCode]: !prevState[prefCode],
+    }));
+  };
+
   return (
     <div className="App">
-      {prefectures.result.map(({ prefCode, prefName }) => (
-        <span key={prefCode}>
-          <label>
-            <input type="checkbox" /> {prefName}
-          </label>
-        </span>
-      ))}
+      <header>Title</header>
+      <section>
+        <h2>都道府県</h2>
+        {prefectures.result.map(({ prefCode, prefName }) => (
+          <span key={prefCode}>
+            <label>
+              <input
+                type="checkbox"
+                checked={checked(prefCode)}
+                onClick={() => toggle(prefCode)}
+              />
+              {prefName}
+            </label>
+          </span>
+        ))}
+      </section>
+      <section>
+        <h2>debug</h2>
+        {JSON.stringify(prefectureCheckState)}
+      </section>
     </div>
   );
 }
